3ee6eb51f05ec738ad924fd98d21325fa5ec7210
Burak Güler <burak.quler@gmail.com>
2024-01-21 23:01:53 +0300

    haber modeli olusturuldu

--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
+from autoslug import AutoSlugField
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    content = models.TextField(default='')
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='news_images/')
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Haber"
+        verbose_name_plural = "Haberler"
