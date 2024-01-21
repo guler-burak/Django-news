106344c4c01e8e462d85fa146ae70ce7c8d0fea5
Burak Güler <burak.quler@gmail.com>
2024-01-21 22:55:23 +0300

    urls ve views duzenlendi

---
+++ news/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('page/', views.page, name='page'),
+    path('author/', views.author, name='author'),
+    path('contact/', views.contact, name='contact'),
+    path('error/', views.error, name='error'),
+    path('index2/', views.index2, name='index2'),
+    path('search/', views.search, name='search'),
+    path('single/', views.single, name='single'),
+    path('single2/', views.single, name='single2'),
+
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'pages/index.html')
+
+def page(request):
+    return render(request, 'pages/page.html')
+
+def author(request):
+    return render(request, 'pages/author.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def error(request):
+    return render(request, 'pages/404.html')
+
+def index2(request):
+    return render(request, 'pages/index-2.html')
+
+def search(request):
+    return render(request, 'pages/search.html')
+
+def single(request):
+    return render(request, 'pages/single.html')
+
+def single2(request):
+    return render(request, 'pages/single2.html')
