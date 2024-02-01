0759ef53333d3a7122e02579c14433f3c142b98d
Burak Güler <burak.quler@gmail.com>
2024-02-01 10:47:58 +0300

    admin paneli duzenlendi

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Sport
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category')
@@ -7,3 +7,8 @@ class NewsAdmin(admin.ModelAdmin):
     ordering = ['-id']
 
 admin.site.register(News, NewsAdmin)
+class SportAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'image', 'slug']
+    search_fields = ['title', 'category']
+
+admin.site.register(Sport, SportAdmin)
