4b6229fcbbd51e6729fcdcf7dfd2790029493a10
Burak Güler <burak.quler@gmail.com>
2024-02-01 11:24:13 +0300

    ekonomi haberleri icin models olusturuldu

--- news/models.py
+++ news/models.py
@@ -30,3 +30,18 @@ class Sport(models.Model):
     class Meta:
         verbose_name = "Spor"
         verbose_name_plural = "Spor"
+
+class Economy(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='news_images/')
+    slug = AutoSlugField(populate_from='category', unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Ekonomi"
+        verbose_name_plural = "Ekonomi"
